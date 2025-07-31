e13f518b39e0eb5a583eb7755428ebd2b8cc10a4
sosaaa123 <lautasosita097@gmail.com>
2025-07-31 19:26:36 -0300

    ahre

--- backenedtecnica/src/main/java/com/tecnica/Main.java
+++ backenedtecnica/src/main/java/com/tecnica/Main.java
@@ -74,6 +74,13 @@ public class Main {
             }
         });
 
+        app.options("{path...}", ctx -> {
+            ctx.header("Access-Control-Allow-Origin", "*");
+            ctx.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+            ctx.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+            ctx.status(204).result("");
+        });
+
         controlador.rutas(app);
 
     
